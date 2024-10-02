2048a3b6f66a8ec05b7c362f6832e7fc7d2c3d99
Sajad-dev720 <sajadzargar.dev@gmail.com>
2024-10-03 00:24:48 +0330

    minor change

--- Dake/Controllers/API/SearchNoticeController.cs
+++ Dake/Controllers/API/SearchNoticeController.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Dake.DAL;
+using Dake.Models;
+using Dake.Service.Interface;
+using Dake.Utility;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Dake.DAL;
-using Dake.Models;
-using Dake.Service.Interface;
-using Newtonsoft.Json.Linq;
-using Dake.Utility;
-using System.IO;
-using DocumentFormat.OpenXml.Office2010.ExcelAc;
-using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Dake.Controllers.API
 {
@@ -21,18 +17,14 @@ namespace Dake.Controllers.API
     public class SearchNoticeController : ControllerBase
     {
         private readonly Context _context;
-        private INotice _notice;
-        private readonly IHostingEnvironment environment;
         private readonly ICategoryImageService _categoryImageService;
 
-        public SearchNoticeController(Context context, INotice notice, IHostingEnvironment environment, 
-            ICategoryImageService categoryImageService)
+        public SearchNoticeController(Context context, ICategoryImageService categoryImageService)
         {
-            this.environment = environment;
             _context = context;
-            _notice = notice;
             _categoryImageService = categoryImageService;
         }
+
         [HttpPost]
         public async Task<object> GetNotices(ProductSearch2 searchNotice)
         {
@@ -59,8 +51,6 @@ namespace Dake.Controllers.API
             pageSize = pageSize > 0 ? pageSize : 10;
             if (!string.IsNullOrEmpty(searchNotice.title))
                 result = result.Where(x => x.title.Contains(searchNotice.title) || (x.description != null && x.description.Contains(searchNotice.title))).ToList();
-
-
 
             if (searchNotice.areaId != 0 && searchNotice.areaId != null)
                 result = result.Where(x => x.areaId == searchNotice.areaId).ToList();
@@ -145,62 +135,5 @@ namespace Dake.Controllers.API
             }
             return subCategories;
         }
-
-        private string getCategoryImage(int catId)
-        {
-            var categoryItem = _context.Categorys.FirstOrDefault(s => s.id == catId);
-            if (!categoryItem.parentCategoryId.HasValue)
-            {
-                return categoryItem.image;
-            }
-            var categoryItem2 = _context.Categorys.FirstOrDefault(s => s.id == categoryItem.parentCategoryId);
-            if (!categoryItem2.parentCategoryId.HasValue)
-            {
-                return categoryItem2.image;
-            }
-            var categoryItem3 = _context.Categorys.FirstOrDefault(s => s.id == categoryItem2.parentCategoryId);
-            if (!categoryItem3.parentCategoryId.HasValue)
-            {
-                return categoryItem3.image;
-            }
-            var categoryItem4 = _context.Categorys.FirstOrDefault(s => s.id == categoryItem3.parentCategoryId);
-            if (!categoryItem4.parentCategoryId.HasValue)
-            {
-                return categoryItem4.image;
-            }
-            var categoryItem5 = _context.Categorys.FirstOrDefault(s => s.id == categoryItem4.parentCategoryId);
-            if (!categoryItem5.parentCategoryId.HasValue)
-            {
-                return categoryItem5.image;
-            }
-            var categoryItem6 = _context.Categorys.FirstOrDefault(s => s.id == categoryItem5.parentCategoryId);
-            if (!categoryItem6.parentCategoryId.HasValue)
-            {
-                return categoryItem6.image;
-            }
-            var categoryItem7 = _context.Categorys.FirstOrDefault(s => s.id == categoryItem6.parentCategoryId);
-            if (!categoryItem7.parentCategoryId.HasValue)
-            {
-                return categoryItem7.image;
-            }
-            var categoryItem8 = _context.Categorys.FirstOrDefault(s => s.id == categoryItem7.parentCategoryId);
-            if (!categoryItem8.parentCategoryId.HasValue)
-            {
-                return categoryItem8.image;
-            }
-            var categoryItem9 = _context.Categorys.FirstOrDefault(s => s.id == categoryItem8.parentCategoryId);
-            if (!categoryItem9.parentCategoryId.HasValue)
-            {
-                return categoryItem9.image;
-            }
-            var categoryItem10 = _context.Categorys.FirstOrDefault(s => s.id == categoryItem9.parentCategoryId);
-            if (!categoryItem10.parentCategoryId.HasValue)
-            {
-                return categoryItem10.image;
-            }
-            return string.Empty;
-        }
-
-
     }
 }
